73810155639a2e41ceb2c8ed344f157cb9b6b0e2
wonyeoe <yeowonlee04@gmail.com>
2025-11-20 21:32:28 +0900

    컴포넌트 테스트용 페이지 정리

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,7 @@ import EVDPage from "./pages/EVD/EVDPage";
 import CBDPage from "./pages/CBD/CBDPage";
 import HOMPage from "./pages/HOM/HOMPage";
 import TestPage from "./pages/TEST/TestPage";
-import TabBarPage from "./pages/COMMON/TabBarPage";
-import SearchBarPage from "./pages/COMMON/SearchBarPage";
-import HeaderPage from "./pages/COMMON/HeaderPage";
 import NotFoundPage from "./pages/NOT/NotFoundPage";
-import MiniCalendarPage from "./pages/COMMON/MiniCalendarPage";
-import EventRowPage from "./pages/EVL/EventRowPage";
-import MainCalendarPage from "./pages/HOM/MainCalendarPage";
-import EventDetailPage from "./pages/EVD/EventDetailPage";
-import ServiceLinkPage from "./pages/COMMON/ServiceLinkPage";
-import ImminentEventsPage from "./pages/HOM/ImminentEventsPage";
-
 
 function App() {
   return (
@@ -30,18 +20,6 @@ function App() {
           <Route index element={<EVLPage />} />
           <Route path="detail/:id" element={<EVDPage />} />
         </Route>
-        <Route path="modules">
-          <Route path="tabBar" element={<TabBarPage />} />
-          <Route path="searchBar" element={<SearchBarPage />} />
-          <Route path="header" element={<HeaderPage />} />
-          <Route path="miniCalendar" element={<MiniCalendarPage/>}/>
-          <Route path="eventRow" element={<EventRowPage />} />        
-          <Route path="mainCalendar" element={<MainCalendarPage/>}/>
-          <Route path="eventDetail" element={<EventDetailPage />} />   
-          <Route path="serviceLink" element={<ServiceLinkPage/>} />
-          <Route path="imminentEvents" element={<ImminentEventsPage/>} />     
-        </Route>
-
         <Route path="test" element={<TestPage />} />
         <Route path="" element={<NotFoundPage />} />
       </Routes>
